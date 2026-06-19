a185ae65311bb8ceaaab108c319478b38d294d86
Muniza Zargar <munizahzargar.iimun@gmail.com>
2026-06-19 21:00:45 +0530

    add technician dashboard view

--- obj/Debug/net8.0/WularItech solutions.AssemblyInfo.cs
+++ obj/Debug/net8.0/WularItech solutions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WularItech solutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6761662e6bc9a126515992fa267fe68be38bba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a2de650ec127e256d62d382c605d9d3d1dd476e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WularItech solutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WularItech solutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
